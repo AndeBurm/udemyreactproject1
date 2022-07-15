9d99afd2c5507bcc9a1f5c493c0b5299c0da200f
AndeBurm <mer4end@gmail.com>
2022-07-15 18:11:02 +0300

    Udemy React Project 1st commit

---
+++ src/components/employee-add-form/employee-add-form.js
@@ -0,0 +1,21 @@
+import './employee-add-form.css'
+
+const EmployeeAddForm = () => {
+    return (
+        <div className="app-add-form">
+            <h3>Добавьте нового сотрудника</h3>
+            <form className="add-form d-flex">
+                <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?"/>
+                <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="ЗП в $?"/>
+                <button type="submit"
+                        className="form-control new-post-label">Добавить</button>
+            </form>
+        </div>
+    )
+}
+
+export default EmployeeAddForm
