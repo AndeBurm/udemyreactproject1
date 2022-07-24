0dae86058225e584939a21762d5a808d33640af2
AndeBurm <mer4end@gmail.com>
2022-07-24 19:19:21 +0300

    project finished

--- src/components/employee-add-form/employee-add-form.js
+++ src/components/employee-add-form/employee-add-form.js
@@ -2,12 +2,10 @@ import {Component} from 'react'
 import './employee-add-form.css'
 
 class EmployeeAddForm extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            salary: ''
-        }
+
+    state = {
+        name: '',
+        salary: ''
     }
 
     onValueChange = (e) => {
@@ -23,6 +21,10 @@ class EmployeeAddForm extends Component{
             name: '',
             salary: ''
         })
+    }
+
+    static onLog = () => {
+        console.log('dot')
     }
 
     render () {
